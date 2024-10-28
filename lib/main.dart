e9468bf7b80775554bd2c5a9582944eb451370fd
mohamedihab <mohamed_ihab.92@hotmail.com>
2024-10-28 20:33:38 +0300

    review

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'firebase_options.dart';
+
+
 import 'package:meal_recommendation_b1/core/routes/app_routes.dart';
 import 'core/utiles/app_themes.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,);
+
+  runApp(const MealApp());
+}
 
 class MealApp extends StatelessWidget {
   const MealApp({super.key});
