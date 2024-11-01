1397a50aecbdd020fe3962e4e9ad09a6751adf4d
MariamAshraf200 <mariamsarhan201@gmail.com>
2024-11-01 22:40:02 +0200

    LoginWithGoogleInFirestore

--- lib/features/auth/login/data/data_source/remote/auth_remote_data_source.dart
+++ lib/features/auth/login/data/data_source/remote/auth_remote_data_source.dart
@@ -1,4 +1,5 @@
- import 'package:firebase_auth/firebase_auth.dart';
+ import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 
@@ -6,8 +7,9 @@ import 'package:google_sign_in/google_sign_in.dart';
 class AuthRemoteDataSource{
   final FirebaseAuth _firebaseAuth;
   final GoogleSignIn _googleSignIn;
+ final FirebaseFirestore _firestore;
 
-  AuthRemoteDataSource(this._firebaseAuth, this._googleSignIn);
+  AuthRemoteDataSource(this._firebaseAuth, this._googleSignIn, this._firestore);
 
   Future<Map<String, dynamic>?> loginWithEmailAndPassword(String email, String password) async {
     UserCredential userCredential = await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
@@ -17,15 +19,37 @@ class AuthRemoteDataSource{
 
 
   Future<Map<String, dynamic>?> loginWithGoogle() async {
-    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-    final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        return null; // The user canceled the sign-in
+      }
 
-    final AuthCredential credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-    UserCredential userCredential = await _firebaseAuth.signInWithCredential(credential);
-    return _getUserData(userCredential.user);
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      UserCredential userCredential = await _firebaseAuth.signInWithCredential(credential);
+      User? user = userCredential.user;
+
+      if (user != null) {
+        // Save user data to Firestore
+        await _firestore.collection('users').doc(user.uid).set({
+          'uid': user.uid,
+          'name': user.displayName,
+          'email': user.email,
+          'profilePhoto': user.photoURL,
+        }, SetOptions(merge: true));
+      }
+
+      return _getUserData(user);
+    } catch (e) {
+      print("Error logging in with Google: $e");
+      return null;
+    }
   }
 
   Future<void> logout() async {
