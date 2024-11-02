6c9eb41121d09eeff1a940d49932d02b54ee1c17
walid barakat <walidmohamedbarakat@gmail.com>
2024-11-02 09:39:56 +0200

    change in main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hive/hive.dart';
+import 'package:meal_recommendation_b1/core/services/di.dart';
+import 'features/home/favorites/data/models/favorites.dart';
+import 'firebase_options.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meal_recommendation_b1/core/routes/app_routes.dart';
 import 'core/utiles/app_themes.dart';
 
-main(){
-  runApp(MealApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,);
+  await Hive.initFlutter();
+  Hive.registerAdapter(FavoritesAdapter());
+  final favoriteBox = await Hive.openBox<Favorites>('favorites');
+
+  await setup(favoriteBox);
+  runApp(const MealApp());
 }
+
 class MealApp extends StatelessWidget {
   const MealApp({super.key});
 
@@ -19,11 +35,9 @@ class MealApp extends StatelessWidget {
         title: 'Meal - Recommendation',
         debugShowCheckedModeBanner: false,
         theme: AppThemes.lightTheme,
-        initialRoute: AppRoutes.home,
+        initialRoute: AppRoutes.splash,
         onGenerateRoute: AppRoutes.generateRoute,
       ),
     );
   }
 }
-
-
